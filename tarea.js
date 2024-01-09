beea6103744c53ffa02564b4ad3de1ee1aff9610
FernandaG <fernanda.guemez@gmail.com>
2024-01-08 18:46:32 -0600

    Se agrega tarea variables terminada

---
+++ tarea.js
@@ -0,0 +1,112 @@
+// Ejercicio para esta sesion
+
+//Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+// Pueden variar entre las palabras reservadas var, let y const
+//  -number
+//  -string
+//  -boolean
+
+
+
+let numeroEdad = 33;
+var anioDeNacimiento = 1990;
+var numeroDeSobrinos = 4;
+let numeroCalle = 43
+let codigoPostal = 90310
+let numeroTelefonico = 9999507421;
+let numeroDeHermanos = 2;
+var aniosEscalando = 10;
+var cantidadDePlantas = 30;
+let numerodeMascostas = 1;
+var animalFavorito = "gato";
+var deporteFavorito = "Escalada";
+let nombreMama = "Matilde";
+let apellidoPapa = "Guemez";
+let apellidoMaterno = "Medina";
+var ciudadDeNacimiento = "Merida";
+let mesDeNacimiento = "septiembre";
+let nombreAmigaFavorita = "Ina";
+let comidaFavorita = "albondigas";
+let musicoMasEscuchado = "fkj"
+let tengoMascota = true;
+var tengoSobrinos = true;
+var tengoHijos = false;
+let vehiculoPropio = true;
+let tienesHermanas = false;
+var estoyCasada = false;
+var eresHijaUnica = false;
+var tienesPerro = false;
+var comesVerduras = true;
+let hablasFrances = false;
+
+
+// Declarar variables numericas y realizar las operaciones aritmeticas basicas
+//      -suma
+//      -resta
+//      -multiplicacion
+//      -division
+
+let numero1 = 100;
+let numero2 = 200;
+let numero3 = 300;
+
+let sumaTarea = numero1 + numero2 + numero3;
+
+let restaTarea = numero2 - numero1
+
+let multiplicacionTarea = numero1 * numero1
+
+let divisionTarea = numero3 / numero1
+
+
+// * Declarar variables que concatenen cadenas de texto
+//      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ 
+
+let saborDulce = "azucar"
+let aromaRico = "flores"
+let contenidoArcoiris = "muchos colores"
+console.log("La formula de las chicassuperpoderosas es:" + " " + saborDulce + " " + aromaRico + " " + "y" + " " + contenidoArcoiris);
+
+
+let estadoDeAnimo = "Feliz"
+let celebracionAnual = "navidad"
+let pensamiento = "prospero"
+let  celebracionAnual2 = "año nuevo"
+console.log( estadoDeAnimo + " " + celebracionAnual+ " " + "y un" + " " + pensamiento + " " + celebracionAnual2);
+
+
+let usuario = "Maria";
+let segundoNombre = "Fernanda";
+let apellidoUsuario = "Guemez";
+let apellidoMama = "Medina";
+console.log( usuario + " " + segundoNombre + " " + apellidoUsuario + " " + apellidoMama);
+
+console.log( "Mi comida favorita es:" + " " + comidaFavorita + " " + "y mi cantante favorito es:" + " " + musicoMasEscuchado);
+console.log( "Mi mama se llama" + " " + nombreMama + " " + "y mi mejor amiga se llama" + " " + nombreAmigaFavorita);
+
+// * Declarar 5 variables y convertirlas a otro tipo de dato
+
+let anioString = String (anioDeNacimiento); // de numero a string
+
+
+let fechaNacimiento = "100990";   // de string a numero
+let fechaNumber = Number (fechaNacimiento);
+
+let  animalbooleano = Boolean (animalFavorito); // a booleano
+
+let edadMascota = 2;
+let edadMascotaString = String (edadMascota);
+
+let numFavorito = "7";
+let numFavNumber = Number (numFavorito)
+
+
+ 
+// *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+// * Imprimir los resultados en pantalla con un console.log()
+
+
+
+
+
