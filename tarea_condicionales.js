59c7eced80cc201d56f57d9a307b4b8217944a77
FernandaG <fernanda.guemez@gmail.com>
2024-01-16 12:42:33 -0600

    Se agregan continuacion de tareas

---
+++ tarea_condicionales.js
@@ -0,0 +1,9 @@
+/** Ejercicio 1
+ * 
+ * Elabora un programa que determine si una persona tiene edad
+ * suficiente para votar
+ * 
+ * Debe recibir como parametro la edad
+ * 
+ * Consideraciones: utilizar la estructura if-else
+ */
