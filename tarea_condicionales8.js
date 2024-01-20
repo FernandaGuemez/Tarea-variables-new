d4a98d702708ffd2427af5df67565ae14c2c1618
FernandaG <fernanda.guemez@gmail.com>
2024-01-19 20:08:36 -0600

    Se agrega tarea 8 de condicionales, usanod if, if else, else

--- tarea_condicionales8.js
+++ tarea_condicionales8.js
@@ -1,4 +1,4 @@
-/** Ejercicio 8
+/* Ejercicio 8
  *
  * Crear una interfaz de un cajero ATM
  *
@@ -12,3 +12,19 @@
  * Segun sea la opcion indicada, imprimir en pantalla la accion a realizar
  * Utilizar if/else
  */
+
+function realizarAccion(opcion) {
+  if (opcion === 1) {
+    console.log("retirar dinero");
+  } else if (opcion === 2) {
+    console.log("hacer transferencia");
+  } else if (opcion === 3) {
+    console.log("deposito");
+  } else if (opcion === 4) {
+    console.log("pago de servicios");
+  } else {
+    console.log("Opcion no valida, escoja de nuevo");
+  }
+}
+
+realizarAccion(1);
